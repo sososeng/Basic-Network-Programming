522835f73b67c9a03aabce800ce6ccea234e0c85
So <sososeng@gmail.com>
2017-12-06 22:11:37 -0600

    adding TCPClient exmaple

---
+++ TCPClient.py
@@ -0,0 +1,16 @@
+import socket
+
+target_host = "www.yahoo.com"
+target_port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((target_host,target_port))
+
+
+
+client.sendall('GET /HTTP/1.1\r\nHost: yahoo.com\r\n\r\n'.encode('utf-8'))
+
+response = client.recv(4096);
+
+print (response)
