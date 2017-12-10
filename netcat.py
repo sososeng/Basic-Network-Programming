31f38fa79d55b05516ded522877ccfd1fac71f46
So <sososeng@gmail.com>
2017-12-09 23:18:46 -0600

    something wrong, fix it later

--- netcat.py
+++ netcat.py
@@ -63,3 +63,39 @@ for o,a in opts:
         port = int(a)
     else:
         assert False, "Unhandled Option"
+
+buffer =sys.stdin.read()
+
+client_sender(buffer)
+if listen:
+    server_loop()
+
+main()
+
+def client_sender(buffer):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client.connect((target,port))
+
+        if len(buffer):
+            client.send(buffer)
+
+        while True:
+            recv_len = 1
+            response = ""
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                response+=data
+                if recv_len < 4096:
+                    break
+
+            print(response)
+            buffer = raw_input("")
+            buffer +="\n"
+            client.send(buffer)
+
+    except:
+        print("[*] Exception! Exiting")
+
+        client.close()
