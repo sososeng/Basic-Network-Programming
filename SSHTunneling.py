2dfce4a3061404da1f3162aa4f461fdff6dcd5df
So <sososeng@gmail.com>
2017-12-16 21:53:15 -0600

    add a method for forwarding request

--- SSHTunneling.py
+++ SSHTunneling.py
@@ -35,4 +35,29 @@ def reverse_forward_tunnel(server_port,remote_host, remote_port, transport):
         thr.setDaemon(True)
         thr.start()
 
-        
+
+def handler(chan, host, port):
+    sock = socket,socket()
+    try:
+        sock.connect((host,port))
+    except Exeption as e:
+        verbose("Forwarding request to %s:%d failed %r" % (host,port,e))
+        return
+    verbose("Connected! Tunnel open %r -> %r ->%r" % (chan.origin_addr, chan.getpeername(),(host,port)))
+
+    while True:
+        r,w,x = select.select([sock,chan],[],[])
+        if sock in r:
+            data = sock.recv(1024)
+            if len(data) == 0:
+                break
+            chan.send(data)
+        if chan in r:
+            data = chan.recv(1024)
+            if len(data) ==0:
+                break
+            sock.send(data)
+
+        chan.close()
+        sock.close()
+        verbose("Tunnel closed from %r" % (chan.origin_addr,))
